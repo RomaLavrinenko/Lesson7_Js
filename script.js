080623b1ae0635ace8ee6fb897bf26068ca7bb90
RomaLavrinenko <73121041+RomaLavrinenko@users.noreply.github.com>
2023-06-23 22:04:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// let person = {
+//     firstName : 'Ivan',
+//     secondName : 'Ivanov',
+//     showData(){
+//         console.log(`${this.firstName} ${this.secondName}`);
+//     }
+// };
+
+// let newPerson = Object.assign({},person);
+// newPerson.firstName = 'Petro';
+// newPerson.secondName = 'Petriv';
+// person.showData();
+// newPerson.showData();
+
+
+
+// function MyMath(){
+//     this.a = 5;
+//     this.b = 2;
+//     this.sum = function(){
+//         console.log(this.a + this.b);
+//     }
+//     this.multiplication  = function(){
+//         console.log(this.a * this.b);
+//     }
+//     this.division = function(){
+//         console.log(this.a / this.b);
+//     }
+//     this.subtraction  = function(){
+//         console.log(this.a - this.b);
+//     }
+// };
+
+// let mymath = new MyMath();
+// mymath.sum();
+// mymath.multiplication();
+// mymath.division();
+// mymath.subtraction();
